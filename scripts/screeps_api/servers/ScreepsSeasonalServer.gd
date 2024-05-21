695556ba8e56c70eb0eed9a5d85483715bc76111
TechTastic <74630543+TechTastic@users.noreply.github.com>
2024-05-21 09:06:25 -0500

    Wiped the Slate

--- scripts/screeps_api/servers/ScreepsSeasonalServer.gd
+++
@@ -1,6 +0,0 @@
-extends ScreepsServer
-class_name ScreepsSeasonalServer
-
-func _init():
-	self.server_name = "Screeps: Seasonal"
-	self.host = "screeps.com/seasonal"
--- scripts/screeps_api/servers/ScreepsWorldServer.gd
+++
@@ -1,6 +0,0 @@
-extends ScreepsServer
-class_name ScreepsWorldServer
-
-func _init():
-	self.server_name = "Screeps: World"
-	self.host = "screeps.com"
